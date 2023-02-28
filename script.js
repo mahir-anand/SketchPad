f1909fc347fe98c89490f474f4e1d7346514098e
mahir118 <mahiranand11@gmail.com>
2023-02-28 10:29:27 -0800

    Add javascript function to update grid text value

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
-const pad = document.getElementById('pad');
+// const pad = document.getElementById('pad');
+// // const color = document.getElementById('color');
+// // const eraser = document.getElementById('eraser');
 
-let size = 16;
+// let size = 70; //picked up from the selection bar
 
-pad.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-pad.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-let numOfGrids = size * size;
+// // initializing grids
+// pad.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+// pad.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+// let numOfGrids = size * size;
+// for (let i = 0 ; i <numOfGrids ; i++) {
+//     let grid = document.createElement('div');
+//     // grid.addEventListener('mouseover', changeColor);
+//     // grid.addEventListener('mousedown', changeColor);
+//     pad.insertAdjacentElement('beforeend',grid);
+// }
 
-for (let i = 0 ; i <numOfGrids ; i++) {
-    let grid = document.createElement('div');
-    pad.insertAdjacentElement('beforeend',grid);
+// // color.addEventListener('click', function () {
+// //     color.style.backgroundColor = 'black';
+// // })
+
+// colorMode();
+
+// function colorMode() {
+//     let grids = document.querySelectorAll('#pad div');
+//     for (const grid of grids) {
+//         grid.addEventListener('click', function() {
+//         grid.style.backgroundColor = 'black'; //picked from color selector
+//         })
+//     }
+// }
+
+// // function eraserMode() {
+// //     let grids = document.querySelectorAll('#pad div');
+// //     for (const grid of grids) {
+// //         grid.addEventListener('click', function() {
+// //         grid.style.backgroundColor = 'white'; //picked from color selector
+// //         })
+// //     }
+// // }
+
+
+
+
+let inputValue = document.querySelector('#inputValue')
+let textValue = document.querySelector('#textValue');
+
+let getValue = function () {    
+    let value = inputValue.value;
+    textValue.innerHTML = value + " X " + value;
 }
 
-changeColor();
-
-function changeColor() {
-    let grids = document.querySelectorAll('#pad div');
-    for (const grid of grids) {
-        grid.addEventListener('click', function() {
-        grid.style.backgroundColor = 'black';
-        })
-    }
-}
-
-
-
-
-
+inputValue.addEventListener('input', () => {
+    getValue();
+})
